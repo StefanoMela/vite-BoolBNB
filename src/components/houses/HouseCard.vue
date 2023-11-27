f0538c64ac4317945106e779e234dbbd4d96d085
Catiflam <gilfrid1@gmail.com>
2023-11-27 13:22:00 +0100

    add axios

---
+++ src/components/houses/HouseCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+	data() {
+		return {};
+	},
+
+	props: {
+		house: Object,
+	},
+
+	methods: {},
+};
+</script>
+
+<template>
+	<div class="container"></div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/houses/HouseFeaturedList.vue
@@ -0,0 +1,33 @@
+<script>
+import HouseCard from "./HouseCard.vue";
+import axios from "axios";
+import { store } from "../../data/store";
+
+export default {
+	data() {
+		return {
+			houses: [],
+		};
+	},
+
+	components: { HouseCard },
+
+	methods: {
+		fetchCards(uri = store.api.baseUrl + "houses") {
+			axios.get(uri).then((response) => {
+				console.log(response);
+				this.houses = response.data;
+			});
+		},
+		created() {
+			this.fetchCards();
+		},
+	},
+};
+</script>
+
+<template>
+	<HouseCard v-for="house in houses" :house="house" />
+</template>
+
+<style lang="scss" scoped></style>
