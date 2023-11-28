8639edd92121ee21a29d4280f14caa766870ddaf
StefanoMela <stefano.mela25@gmail.com>
2023-11-28 15:12:07 +0100

    logo link change

--- src/components/ui/NavbarUi.vue
+++ src/components/ui/NavbarUi.vue
@@ -1,37 +1,45 @@
 <script>
 export default {
-	data() {
-		return {};
-	},
+  data() {
+    return {};
+  },
 };
 </script>
 
 <template>
-	<nav class="navbar navbar-expand-lg bg-light">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Navbar</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<a class="nav-link" href="http://localhost:8000/login">Login</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="http://localhost:8000/register">Register</a>
-					</li>
-					<li class="nav-item">
-						<RouterLink 
-						class="nav-link" 
-						:to="{name: 'search'}"
-						>Cerca
-					</RouterLink>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="http://localhost:5173/">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost:8000/login">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost:8000/register"
+              >Register</a
+            >
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" :to="{ name: 'search' }"
+              >Cerca
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style lang="scss" scoped></style>
