5b7343208f917b46b997557b6754c2edce67d3fb
StefanoMela <stefano.mela25@gmail.com>
2023-11-28 15:09:21 +0100

    add logic

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,15 +1,56 @@
 <script>
+import { store } from "../data/store";
+import axios from "axios";
+
+
 export default {
+    data(){
+        return{
+            store,
+            searchField: '',
+        }
+    },
+
+    methods: {
+
+        postSearch(){
+
+            if(
+                !this.searchField
+            )
+            return
+            const data = this.searchField
+            axios.post(store.api.baseUrl + 'search', data)
+            .then((response)=>{
+                console.log(response);
+            })
+        }
+        
+    }
 };
 </script>
 
 <template>
-    <h4>Cerca qui la tua casa dei sogni!</h4>
+  <h4>Cerca qui la tua casa dei sogni!</h4>
+  <form class="d-flex" role="search">
+    <input
+      class="form-control me-2"
+      type="search"
+      placeholder="Search"
+      aria-label="Search"
+      v-model="searchField"
+    />
+    <button
+    @click="postSearch()"
+    class="btn btn-outline-success" 
+    type="submit">Search</button>
+  </form>
 </template>
 
 <style lang="scss" scoped>
-h4{
+h4 {
   text-align: center;
   margin-top: 3rem;
   margin-bottom: 3rem;
-}</style>
+}
+</style>
