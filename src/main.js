7700a36e2d8453510118cb3d8c3374477f933c70
VincenzoDePetris <vince02@live.it>
2023-11-29 17:31:45 +0100

    add: show

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+import App from "./App.vue";
 import { createApp } from "vue";
 import "./assets/scss/style.scss";
 
 // IMPORT ROUTER
- import { router } from "./router";
+import { router } from "./router";
 
 // IMPORT BOOTSTRAP
 import * as bootstrap from "bootstrap";
@@ -14,8 +15,28 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
 import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
-import App from "./App.vue";
+import { faKitchenSet } from "@fortawesome/free-solid-svg-icons";
+import { faWifi } from "@fortawesome/free-solid-svg-icons";
+import { faSeedling } from "@fortawesome/free-solid-svg-icons";
+import { faSquareParking } from "@fortawesome/free-solid-svg-icons";
+import { faTv } from "@fortawesome/free-solid-svg-icons";
+import { faPersonSwimming } from "@fortawesome/free-solid-svg-icons";
+import { faWind } from "@fortawesome/free-solid-svg-icons";
+import { faWater } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App)
-.use(router)
-.mount("#app");
+library.add(
+  faUserSecret,
+  faKitchenSet,
+  faWifi,
+  faSeedling,
+  faSquareParking,
+  faTv,
+  faPersonSwimming,
+  faWind,
+  faWater
+);
+
+const app = createApp(App);
+app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
