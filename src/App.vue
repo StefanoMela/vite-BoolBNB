55488c9b152aa9821526754ebbe46c3fd4fad1ba
VincenzoDePetris <vince02@live.it>
2023-11-30 18:54:21 +0100

    R

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@ import NavbarUi from "./components/ui/NavbarUi.vue";
 import { RouterView } from "vue-router";
 
 export default {
-	components: { NavbarUi, routerView },
+  components: { NavbarUi, RouterView },
 };
 </script>
 
 <template>
-	<NavbarUi />
-	<div class="container">
-		<router-view></router-view>
-	</div>
+  <NavbarUi />
+  <div class="container">
+    <router-view></router-view>
+  </div>
 </template>
 
 <style></style>
